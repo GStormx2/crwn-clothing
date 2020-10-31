7db262d10dad32fb9d9d20dece05527ec97a05b7
Tanqir Masroor <tanqir.msarker@gmail.com>
2020-11-01 00:48:17 +0600

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello
+        <HomePage> </HomePage>
       </div>
     );
   }
