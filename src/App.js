677849f9a869dd0570f11038d082e52246be83e2
Tanqir Masroor <tanqir.msarker@gmail.com>
2020-11-04 15:38:51 +0600

    routing with react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route} from 'react-router-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <HomePage> </HomePage>
-      </div>
-    );
-  }
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+}
+
+function App(){
+  return(
+    <div>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={HatsPage} />
+    </div>
+  );
 }
 
 export default App;
